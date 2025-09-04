260bf074e2ba4caea303cf5b48645a981f5f6d61
Angel Gonzalez <usftechacg@gmail.com>
2025-09-04 12:21:34 -0400

    File Removal
    Removing files not present int he next version

--- src/Contracts/RequestFactoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ProjectSaturnStudios\Xfer\Contracts;
-
-interface RequestFactoryInterface
-{
-    public function make(): TransferRequestInterface;
-}
--- src/Contracts/TransferActionInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ProjectSaturnStudios\Xfer\Contracts;
-
-interface TransferActionInterface
-{
-    public function transfer(TransferRequestInterface $request): TransferResultInterface;
-}
